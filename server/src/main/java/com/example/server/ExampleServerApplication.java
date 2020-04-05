871307748951491870f0c79d930dd6ff3ebb3bdd
borisov <a.borisov@8bitgroup.com>
2020-04-06 01:06:40 +0300

    example server

---
+++ server/src/main/java/com/example/server/ExampleServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.server;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@ComponentScan("org.camunda.bpm.engine.grpc.server.**")
+@EnableTransactionManagement
+@EnableProcessApplication("grpc-example")
+public class ExampleServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleServerApplication.class, args);
+    }
+}
